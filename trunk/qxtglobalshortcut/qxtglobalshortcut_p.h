535575b4d2aea322f5858ea11c13fea4d02102a5
Alvin.rxg@gmail.com <Alvin.rxg@gmail.com@914bc194-7d5f-f514-21fb-d6a79598739a>
2011-10-29 08:44:42 +0000

    same as r6, update syntax in subfolder qxtglobalshortcut

--- trunk/qxtglobalshortcut/qxtglobalshortcut_p.h
+++ trunk/qxtglobalshortcut/qxtglobalshortcut_p.h
@@ -32,32 +32,32 @@
 
 class QxtGlobalShortcutPrivate : public QxtPrivate<QxtGlobalShortcut>
 {
-public:
-    QXT_DECLARE_PUBLIC(QxtGlobalShortcut)
-    QxtGlobalShortcutPrivate();
-    ~QxtGlobalShortcutPrivate();
+	public:
+		QXT_DECLARE_PUBLIC(QxtGlobalShortcut)
+			QxtGlobalShortcutPrivate();
+		~QxtGlobalShortcutPrivate();
 
-    bool enabled;
-    Qt::Key key;
-    Qt::KeyboardModifiers mods;
+		bool enabled;
+		Qt::Key key;
+		Qt::KeyboardModifiers mods;
 
-    bool setShortcut(const QKeySequence& shortcut);
-    bool unsetShortcut();
+		bool setShortcut(const QKeySequence& shortcut);
+		bool unsetShortcut();
 
-    static bool error;
-    static int ref;
-    static QAbstractEventDispatcher::EventFilter prevEventFilter;
-    static bool eventFilter(void* message);
+		static bool error;
+		static int ref;
+		static QAbstractEventDispatcher::EventFilter prevEventFilter;
+		static bool eventFilter(void* message);
 
-private:
-    static quint32 nativeKeycode(Qt::Key keycode);
-    static quint32 nativeModifiers(Qt::KeyboardModifiers modifiers);
+	private:
+		static quint32 nativeKeycode(Qt::Key keycode);
+		static quint32 nativeModifiers(Qt::KeyboardModifiers modifiers);
 
-    static bool registerShortcut(quint32 nativeKey, quint32 nativeMods);
-    static bool unregisterShortcut(quint32 nativeKey, quint32 nativeMods);
-    static void activateShortcut(quint32 nativeKey, quint32 nativeMods);
+		static bool registerShortcut(quint32 nativeKey, quint32 nativeMods);
+		static bool unregisterShortcut(quint32 nativeKey, quint32 nativeMods);
+		static void activateShortcut(quint32 nativeKey, quint32 nativeMods);
 
-    static QHash<QPair<quint32, quint32>, QxtGlobalShortcut*> shortcuts;
+		static QHash<QPair<quint32, quint32>, QxtGlobalShortcut*> shortcuts;
 };
 
 #endif // QXTGLOBALSHORTCUT_P_H
